e38b7af45b84ce41da8087f76798cfdd0ae633b2
Ben Hodgson <ben.hodgson@songkick.com>
2015-11-04 12:08:42 +0000

    Switch to postcss js api

---
+++ postcss.js
@@ -0,0 +1,28 @@
+var fs = require('fs'),
+    postcss = require('postcss'),
+    partialImport = require('postcss-partial-import')(),
+    customProperties = require('postcss-custom-properties')(),
+    autoprefixer = require('autoprefixer')(),
+    cssnano = require('cssnano')()
+
+var src = 'src/core.css',
+    dest = 'dist/core.css';
+
+var css = fs.readFileSync(src, 'utf8');
+
+console.log(css);
+
+postcss([
+    partialImport,
+    customProperties,
+    autoprefixer,
+    cssnano
+])
+.process(css, {
+    from: src,
+    to: dest
+})
+.then(function (result) {
+    fs.writeFileSync(dest, result.css);
+    if (result.map) fs.writeFileSync(dest + '.map', result.map)
+});
